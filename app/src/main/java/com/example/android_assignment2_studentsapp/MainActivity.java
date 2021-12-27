617252a64d14037eb2a6de60135956429793e369
Anthony E <e4nthony@gmail.com>
2021-12-27 19:30:35 +0200

    adapter started - MainActivity.java

--- app/src/main/java/com/example/android_assignment2_studentsapp/MainActivity.java
+++ app/src/main/java/com/example/android_assignment2_studentsapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android_assignment2_studentsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }

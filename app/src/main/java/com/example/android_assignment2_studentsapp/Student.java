20b5a509f6d3ce30782fbd1de8682eecc8c8ad09
Anthony E <e4nthony@gmail.com>
2021-12-27 21:40:21 +0200

    added getters&setters to Student object

--- app/src/main/java/com/example/android_assignment2_studentsapp/Student.java
+++ app/src/main/java/com/example/android_assignment2_studentsapp/Student.java
@@ -1,7 +1,33 @@
 package com.example.android_assignment2_studentsapp;
 
 public class Student {
-    String name = "";
-    int id = -1;
-    boolean cb = false;
+    private String name = "";
+    private int id = -1;
+    private boolean cb = false;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isCb() {
+        return cb;
+    }
+
+    public void setCb(boolean cb) {
+        this.cb = cb;
+    }
+
+
 }
